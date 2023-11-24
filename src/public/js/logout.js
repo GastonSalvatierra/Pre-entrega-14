94b2501897a2a304e7f0f9a63b0aa904fbff7555
Gaston Salvatierra <gastonsalvatierra@icloud.com>
2023-11-24 17:06:29 -0300

    Pre-entrega-14

---
+++ src/public/js/logout.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const form = document.getElementById('closeAll');
+const addProduct = document.querySelectorAll('.addProduct');
+
+form.addEventListener('click', () => {
+    // Realiza una solicitud al servidor para destruir la sesión
+    fetch('/api/products/pages/1', {
+        method: 'POST', // Puedes utilizar el método POST para enviar una solicitud al servidor para destruir la sesión
+    })
+    .then(response => {
+        if (response.ok) {
+            // Si la solicitud fue exitosa, redirige a la página de inicio de sesión
+            window.location.replace('/'); // Corrección aquí
+        } else {
+            console.error('Error al cerrar sesión');
+        }
+    })
+    .catch(error => {
+        console.error('Error al cerrar sesión:', error);
+    });
+});
+
+
+addProduct.forEach(boton => {
+    boton.addEventListener('click', () => {
+
+    alert('Producto agregado con exito');
+
+    });
+});
+
+
+   
+});
+
+
+
+
---
+++ src/routes/users.router.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+const router= Router();
+
+router.get("/premium/:uid");
+
+
+
+
+export default router;
